b4ed3ab71e1130ffc2106afad2d9d959ec966b6f
DeyvidJLira <deyvidjlira@gmail.com>
2016-09-24 20:45:35 -0300

    Update CloudSpawner to position the player

--- Assets/Scripts/Cloud Controller/CloudSpawner.cs
+++ Assets/Scripts/Cloud Controller/CloudSpawner.cs
@@ -10,21 +10,23 @@ public class CloudSpawner : MonoBehaviour {
 	private float m_DistanceBetweenClouds = 3f;
 	// Minimum and maximum of the position horizontal
 	private float m_MinX, m_MaxX;
-	// Control position x to be generated
-	private int m_ControlX = 0;
+    // Control position x to be generated
+    private int m_ControlX;
 	// Position vertical of the last cloud
 	private float m_LastCloudPositionY;
     // Player
     private GameObject m_Player;
 
 	void Awake() {
+        m_ControlX = 0;
 		SetMinAndMaxX ();
         GenerateClouds();
+        m_Player = GameObject.FindGameObjectWithTag("Player");
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        PositionThePlayer();	
 	}
 	
 	// Update is called once per frame
@@ -83,4 +85,29 @@ public class CloudSpawner : MonoBehaviour {
 		}
 	}
 
+    // Position the player in the first cloud
+    void PositionThePlayer() {
+        GameObject[] darkClouds = GameObject.FindGameObjectsWithTag("DarkCloud");
+        GameObject[] clouds = GameObject.FindGameObjectsWithTag("Cloud");
+        
+        for(int i = 0; i < darkClouds.Length; i++) {
+            if(darkClouds[i].transform.position.y == 0f) {
+                Vector3 tempDarkCloudPosition = darkClouds[i].transform.position;
+                darkClouds[i].transform.position = clouds[0].transform.position;
+                clouds[0].transform.position = tempDarkCloudPosition;
+            }
+        }
+
+        Vector3 temp = clouds[0].transform.position;
+
+        for(int i = 1; i < clouds.Length; i++) {
+            if (temp.y < clouds[i].transform.position.y) {
+                temp = clouds[i].transform.position;
+            }
+        }
+
+        temp.y += 0.8f;
+
+        m_Player.transform.position = temp;
+    }
 }
