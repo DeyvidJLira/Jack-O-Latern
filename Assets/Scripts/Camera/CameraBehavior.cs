f18f0c94a68be99d0f1ed62b1abf52cbe9b82894
DeyvidJLira <deyvidjlira@gmail.com>
2016-09-24 21:05:38 -0300

    Create script CameraBehavior

---
+++ Assets/Scripts/Camera/CameraBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+    private float m_Speed = 1f;
+    private float m_Acceleration = 0.2f;
+    private float m_SpeedMax = 3.2f;
+
+    [HideInInspector]
+    private bool m_CanMoveCamera;
+
+	// Use this for initialization
+	void Start () {
+        m_CanMoveCamera = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_CanMoveCamera)
+            MoveCamera();
+	}
+
+    void MoveCamera() {
+        Vector3 temp = transform.position;
+
+        float oldY = temp.y;
+
+        float newY = temp.y - (m_Speed * Time.deltaTime);
+
+        temp.y = Mathf.Clamp(temp.y, oldY, newY);
+
+        transform.position = temp;
+
+        m_Speed += m_Acceleration * Time.deltaTime;
+
+        if (m_Speed > m_SpeedMax)
+            m_Speed = m_SpeedMax;
+    }
+}
