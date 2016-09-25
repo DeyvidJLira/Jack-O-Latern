bf524a9889a3342f09a1a77313ee7a3de1a94923
DeyvidJLira <deyvidjlira@gmail.com>
2016-09-25 00:19:24 -0300

    Create Background Controller

---
+++ Assets/Scripts/Background Controller/BackgroundSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSpawner : MonoBehaviour {
+
+    private GameObject[] m_Backgrounds;
+    private float m_LastPositionY;
+
+	// Use this for initialization
+	void Start () {
+        GetBackgroundAndSetLastY();
+	}
+
+    void GetBackgroundAndSetLastY() {
+        m_Backgrounds = GameObject.FindGameObjectsWithTag("Background");
+
+        m_LastPositionY = m_Backgrounds[0].transform.position.y;
+
+        for(int i = 1; i < m_Backgrounds.Length; i++) {
+            if (m_LastPositionY > m_Backgrounds[i].transform.position.y)
+                m_LastPositionY = m_Backgrounds[i].transform.position.y;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D target) {
+        if(target.tag == "Background") {
+            if(target.transform.position.y == m_LastPositionY) {
+                Vector3 temp = target.transform.position;
+                float height = ((BoxCollider2D)target).size.y;
+                for(int i = 0; i < m_Backgrounds.Length; i++) {
+                    if(!m_Backgrounds[i].activeInHierarchy) {
+                        temp.y -= height;
+
+                        m_LastPositionY = temp.y;
+
+                        m_Backgrounds[i].transform.position = temp;
+                        m_Backgrounds[i].SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
