b31acc4dc1d40c8dfc4111a54cb7374ae8782e36
DeyvidJLira <deyvidjlira@gmail.com>
2016-09-29 21:20:30 -0300

    Update Options with GamePreferencesManager

--- Assets/Scripts/UI Controller/OptionsController.cs
+++ Assets/Scripts/UI Controller/OptionsController.cs
@@ -4,10 +4,48 @@ using System.Collections;
 
 public class OptionsController : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject m_EasySign, m_MediumSign, m_HardSign;
+
+    private GameObject m_LastActived = null;
+
+
 	// Use this for initialization
 	void Start () {
-	
+        m_LastActived = SetDifficulty(GamePreferencesManager.m_Instance.m_LevelDifficulty);
 	}
+
+    public void ChooseEasyDifficulty() {
+        m_LastActived = SetDifficulty(LevelDifficulty.EASY);
+    }
+
+    public void ChooseMediumDifficulty() {
+        m_LastActived = SetDifficulty(LevelDifficulty.MEDIUM);
+    }
+
+    public void ChooseHardDifficulty() {
+        m_LastActived = SetDifficulty(LevelDifficulty.HARD);
+    }
+
+
+    private GameObject SetDifficulty(LevelDifficulty levelDifficulty) {
+        if(m_LastActived != null)
+            m_LastActived.SetActive(false);
+        GamePreferencesManager.m_Instance.ConfigDifficulty(levelDifficulty);
+        switch (levelDifficulty) {
+            case LevelDifficulty.EASY:
+                m_EasySign.SetActive(true);
+                return m_EasySign;
+            case LevelDifficulty.MEDIUM:
+                m_MediumSign.SetActive(true);
+                return m_MediumSign;
+            case LevelDifficulty.HARD:
+                m_HardSign.SetActive(true);
+                return m_HardSign;
+            default:
+                return null;
+        }
+    }
 
     public void BackToMainMenu() {
         SceneManager.LoadScene("MainMenu");
