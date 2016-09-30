1f15eb22aba8f9d2e7a2d4ba694312ef4460ec41
DeyvidJLira <deyvidjlira@gmail.com>
2016-09-29 21:19:40 -0300

    Update Highscore with GamePreferencesManager

--- Assets/Scripts/UI Controller/HighscoreController.cs
+++ Assets/Scripts/UI Controller/HighscoreController.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HighscoreController : MonoBehaviour {
 
+    [SerializeField]
+    private Text m_HighscoreText, m_CoinHighscoreText;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        SetHighscore(GamePreferencesManager.m_Instance.m_Highscore, GamePreferencesManager.m_Instance.m_Coin_Highscore);
+    }
+
+    void SetHighscore(int highscore, int coinHighscore) {
+        m_HighscoreText.text = highscore.ToString();
+        m_CoinHighscoreText.text = coinHighscore.ToString();
+    }
 	
     public void BackToMainMenu() {
         SceneManager.LoadScene("MainMenu");
