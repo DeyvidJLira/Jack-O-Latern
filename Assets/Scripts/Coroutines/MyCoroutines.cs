77f5d2299c873d351cf7c2ff1c4335bf683a0a79
DeyvidJLira <deyvidjlira@gmail.com>
2016-11-26 13:05:41 -0200

    Create coroutine WaitForRealSeconds

---
+++ Assets/Scripts/Coroutines/MyCoroutines.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MyCoroutines {
+    
+    public static IEnumerator WaitForRealSeconds(float time) {
+        float timeStart = Time.realtimeSinceStartup;
+
+        while(Time.realtimeSinceStartup < (timeStart + time)) {
+            yield return null;
+        }
+    }
+	
+}
